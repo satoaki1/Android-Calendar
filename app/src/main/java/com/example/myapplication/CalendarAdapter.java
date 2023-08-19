ad746a35014a417e0d1e22e8d8c7439c1f42fa60
ihsa332019 <satoakiishihara@sd.taylors.edu.my>
2023-08-19 19:50:21 +0900

    Added a function to display a number of days in current calendar view which is divisible by 7. Added comments to save detailed explanation of each codes in MainActivity.java and CalendarAdapter.java. Adjusted verticalSpacing in activity_main.xml to change the view.

--- app/src/main/java/com/example/myapplication/CalendarAdapter.java
+++ app/src/main/java/com/example/myapplication/CalendarAdapter.java
@@ -14,18 +14,28 @@ import java.util.Date;
 import java.util.List;
 
 public class CalendarAdapter extends BaseAdapter {
-    private final List<Date> dates;
-    private final Context context;
-    private final Calendar currentMonthDate;
-//    private final SimpleDateFormat dateFormat = new SimpleDateFormat("dd", Locale.getDefault());
 
+    // List of dates to be displayed on the calendar
+    private final List<Date> dates;
+
+    // Context object, used to get application-specific resources and services
+    private final Context context;
+
+    // Current month's Calendar object
+    private final Calendar currentMonthDate;
+
+    // Constructor for the CalendarAdapter class
     public CalendarAdapter(Context context, List<Date> dates) {
+        // Set the context and dates from the parameters
         this.context = context;
         this.dates = dates;
+
+        // Initialize the current month's Calendar object and set it to today's date
         this.currentMonthDate = Calendar.getInstance();
         this.currentMonthDate.set(LocalDate.now().getYear(), getMonth(LocalDate.now().getMonth()), LocalDate.now().getDayOfMonth());
     }
 
+    // Convert the provided Month enum to a corresponding integer value
     public int getMonth(Month currentMonth) {
         int month = 0;
         switch (currentMonth) {
@@ -69,16 +79,19 @@ public class CalendarAdapter extends BaseAdapter {
         return month;
     }
 
+    // Get the total count of dates to be displayed
     @Override
     public int getCount() {
         return dates.size();
     }
 
+    // Get the Date object at the specified position
     @Override
     public Object getItem(int position) {
         return dates.get(position);
     }
 
+    // Return the position as the item ID
     @Override
     public long getItemId(int position) {
         return position;
@@ -86,41 +99,53 @@ public class CalendarAdapter extends BaseAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
+        // Try to reuse an existing view (convertView). If it doesn't exist, create a new TextView.
         TextView dayView = (TextView) convertView;
         if (dayView == null) {
             dayView = new TextView(context);
         }
 
+        // Get the date for the current position
         Date date = (Date) getItem(position);
+
+        // Convert the Date to a Calendar object for easier manipulation
         Calendar dayDate = Calendar.getInstance();
         dayDate.setTime(date);
 
-        // Set day number as text
+        // Get the day number from the date (e.g., 1, 2, 3,...31) and set it to the TextView
         int dayNumber = dayDate.get(Calendar.DAY_OF_MONTH);
         dayView.setText(String.valueOf(dayNumber));
 
+        // Customize the appearance of the TextView based on the date
         customizeDayView(dayView, dayDate);
 
+        // Return the customized TextView
         return dayView;
     }
 
     public void customizeDayView(View dayView, Calendar dayDate) {
+        // Get the day of the week for the given date (e.g., Sunday, Monday,...Saturday)
         int dayOfWeek = dayDate.get(Calendar.DAY_OF_WEEK);
 
-        // Check if day is Saturday or Sunday first
+        // If the day is Saturday, set its background to blue and exit the method
         if (dayOfWeek == Calendar.SATURDAY) {
             dayView.setBackgroundColor(Color.BLUE);
-            return; // exit the method after setting the color
-        } else if (dayOfWeek == Calendar.SUNDAY) {
+            return;
+        }
+        // If the day is Sunday, set its background to red and exit the method
+        else if (dayOfWeek == Calendar.SUNDAY) {
             dayView.setBackgroundColor(Color.RED);
-            return; // exit the method after setting the color
+            return;
         }
 
-        // Check if day is from previous, current, or next month
+        // Check if the date is from a month other than the current month. If so, set its background to gray.
+        // For example, if displaying January and a date from December or February is shown, it will be grayed out.
         if (dayDate.get(Calendar.MONTH) < currentMonthDate.get(Calendar.MONTH) ||
                 dayDate.get(Calendar.MONTH) > currentMonthDate.get(Calendar.MONTH)) {
             dayView.setBackgroundColor(Color.GRAY);
-        } else {
+        }
+        // If the date is from the current month, set its background to white.
+        else {
             dayView.setBackgroundColor(Color.WHITE);
         }
     }
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -16,52 +16,101 @@ import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
+    // TextView to display the current month and days in the month
     private TextView currentMonthTextView;
+
+    // Calendar object to track the current month and days in the month
     private Calendar currentCalendar;
+
+    // GridView to display calendar's dates
     private GridView calendarGridView;
+
+    // SimpleDateFormat to format current month and year
     private final SimpleDateFormat monthFormat = new SimpleDateFormat("MMMM yyyy", Locale.getDefault());
 
+
+    /**
+     * @author Satoaki Ishihara
+     * This is the method that would be implemented in default when the application is launched.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Set the layout for this activity (defined in "activity_main.xml")
         setContentView(R.layout.activity_main);
 
+        // Initialize the currentCalendar variable with the current date and time
         currentCalendar = Calendar.getInstance();
 
+        // Find and assign each items from the layout by Id
+        // - Find and assign the TextView that displays the current month
+        // - Find and assign the GridView that will display the days of the month
+        // - Find and assign the Button that will move to the previous month when clicked
+        // - Find and assign the Button that will move to the next month when clicked
         currentMonthTextView = findViewById(R.id.currentMonthTextView);
         calendarGridView = findViewById(R.id.calendarGridView);
         Button prevMonthButton = findViewById(R.id.prevMonthButton);
         Button nextMonthButton = findViewById(R.id.nextMonthButton);
 
+        // Load the current month's dates and display them in the GridView
         loadMonth();
 
+        // Once Prev button is clicked, display the month which value is -1 from current month
         prevMonthButton.setOnClickListener(v -> {
             currentCalendar.add(Calendar.MONTH, -1);
             loadMonth();
         });
 
+        // Once Next button is clicked, display the month which value is +1 from current month
         nextMonthButton.setOnClickListener(v -> {
             currentCalendar.add(Calendar.MONTH, 1);
             loadMonth();
         });
     }
 
+    /**
+     * @author Satoaki Ishihara
+     * This is the method to load the current month's dates and display them in the GridView,
+     * in onCreate() method written above.
+     */
     private void loadMonth() {
+        // Set the text of the current month and year to the TextView
         currentMonthTextView.setText(monthFormat.format(currentCalendar.getTime()));
 
+        // Create a list to hold dates for the current month view
         List<Date> dates = new ArrayList<>();
+
+        // Clone the current calendar to avoid modifying the original
         Calendar monthCalendar = (Calendar) currentCalendar.clone();
+
+        // Set the cloned calendar to the 1st day of the month
         monthCalendar.set(Calendar.DAY_OF_MONTH, 1);
+
+        // Calculate the day of the week for the 1st day (0 = Sunday, 1 = Monday, etc.)
         int firstDayOfMonth = monthCalendar.get(Calendar.DAY_OF_WEEK) - 1;
+
+        // Move the calendar back to the start of the week (could be a day from the previous month)
         monthCalendar.add(Calendar.DAY_OF_MONTH, -firstDayOfMonth);
 
-        int maxDaysInMonth = currentCalendar.getActualMaximum(Calendar.DAY_OF_MONTH) + firstDayOfMonth;
+        // Calculate days needed at the end of the month to make the last row contain 7 days
+        int daysInCurrentMonth = currentCalendar.getActualMaximum(Calendar.DAY_OF_MONTH);
+        int daysNeededAtEnd = 7 - ((daysInCurrentMonth + firstDayOfMonth) % 7);
 
-        while (dates.size() < maxDaysInMonth) {
-            dates.add(monthCalendar.getTime());
-            monthCalendar.add(Calendar.DAY_OF_MONTH, 1);
+        // Adjust if it's already a complete week
+        if (daysNeededAtEnd == 7) {
+            daysNeededAtEnd = 0;
         }
 
+        int totalDays = daysInCurrentMonth + firstDayOfMonth + daysNeededAtEnd; // Total nodes to show in the grid
+
+        // Fill the dates list with days for the month view
+        while (dates.size() < totalDays) {
+            dates.add(monthCalendar.getTime());
+            monthCalendar.add(Calendar.DAY_OF_MONTH, 1);  // Move to the next day
+        }
+
+        // Set the GridView's adapter with the list of dates
         calendarGridView.setAdapter(new CalendarAdapter(this, dates));
     }
 }
